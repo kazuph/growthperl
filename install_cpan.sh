0cbfd1274810ba6f3dc5559853ebf9e3715a3075
Kazuhiro Homma <kazu.homma@gmail.com>
2012-12-08 03:01:59 -0500

    cpanインストールコマンド修正

--- install_cpan.sh
+++ install_cpan.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#cpanm Module::CoreList
-#wait
+# cpanm Module::CoreList
+# wait
 cpanm -Lextlib -n --installdeps .
-wait
-perl -Mlib=extlib/lib/perl5 extlib/bin/plackup -s Starman -E production --preload-app app.psgi
+# wait
+# perl -Mlib=extlib/lib/perl5 extlib/bin/plackup -s Starman -E production --preload-app app.psgi
 
