c4102f9cac6eebfb6f41f4bff9274a8ee46fe89c
Kazuhiro Homma <kazu.homma@gmail.com>
2013-01-30 22:11:56 +0900

    carton使った状態でstarman起動するコマンド

--- bin/run_server_for_development.sh
+++ bin/run_server_for_development.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-perl -Mlib=extlib/lib/perl5 extlib/bin/plackup -s Starman -E development --preload-app --disable-keepalive --workers 2 app.psgi -r
+# perl -Mlib=extlib/lib/perl5 extlib/bin/plackup -s Starman -E development --preload-app --disable-keepalive --workers 2 app.psgi -r
+carton exec -Ilib -- starman -E development --preload-app --disable-keepalive --workers 2 app.psgi -r
