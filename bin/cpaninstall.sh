55984c6fd3d37ad1a58e3b17d7d4b1ade1b9432a
Kazuhiro Homma <kazu.homma@gmail.com>
2013-03-13 08:46:08 +0900

    大文字

--- bin/cpaninstall.sh
+++ bin/cpaninstall.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-cpanm carton
+cpanm Carton
 # plenv
 if [ -d ${HOME}/.plenv  ] ; then
     plenv rehash
 fi
+
 carton install
 # cpanm -Lextlib -n --installdeps .
+
 cpanm Test::Pretty
 cpanm Proclet
 
